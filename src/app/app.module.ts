c16ca6f61febea57adc1172061eb803f378ec92f
moreaudamien <moreau_student2023@wilder.school>
2023-06-15 11:22:39 +0200

    11/12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,28 @@ import { AppComponent } from './app.component';
 import { AppCreateKittenComponent } from './app-create-kitten/app-create-kitten.component';
 import { AppListKittenComponent } from './app-list-kitten/app-list-kitten.component';
 import { AppUserKittenComponent } from './app-user-kitten/app-user-kitten.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/list-kitten', pathMatch: 'full' },
+  { path: 'create-kitten', component: AppCreateKittenComponent },
+  { path: 'list-kitten', component: AppListKittenComponent },
+  { path: 'user-kitten', component: AppUserKittenComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     AppCreateKittenComponent,
     AppListKittenComponent,
-    AppUserKittenComponent
+    AppUserKittenComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes), 
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
